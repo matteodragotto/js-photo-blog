8a0a9c23998ccff78149672e210d393da6a1db36
matteodragotto <matteodrag8@gmail.com>
2024-12-05 11:57:12 +0100

    [feat] milestone 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 const endpoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
+const row = document.querySelector('.row')
+
+
+axios.get(endpoint)
+ .then (response => {
+  console.log(response.data);  
+  row.innerHTML = ''
+  response.data.forEach(photo => printCard(photo))
+ })
+
+
+
+
+
+ function printCard(photo) {
+  const {title, url} = photo
+  console.log(title, url);
+  
+  row.innerHTML += `
+  <div class="col-4">
+          <div class="user-card">
+            <img src="./assets/img/pin.svg" alt="pin" class="pin">
+            <div class="image">
+              <img src="${url}" alt="${url}">
+            </div>
+            <div class="text d-flex justify-content-start">
+              <p>${title}</p>
+            </div>
+          </div>
+        </div>    `
+ }
